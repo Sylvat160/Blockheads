cc21002f931808a94de41f29d731420733113536
Sylvain T <sylvaintagnabou@gmail.com>
2023-05-21 23:11:59 +0000

    (add) : set routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,8 +15,9 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path='/' element={ <Home /> } />
-          {/* <Route path='' element />
-          <Route path='' element /> */}
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-campaign' element={<CreateCampaign />} />
+          <Route path='/campaign-details/:id' element={<CampaignDetails />} />
         </Routes>
       </div>
     </div>
