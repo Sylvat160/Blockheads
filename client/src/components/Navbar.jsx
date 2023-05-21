fc3b2f498d6daf6ae9ed431d00978a671d8e1c26
Sylvain T <sylvaintagnabou@gmail.com>
2023-05-21 23:03:10 +0000

    (done) : Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import { navlinks } from '../constants'
 
 const Navbar = () => {
   const navigate = useNavigate()
-  const [isActivated, setIsActivated] = useState('dashboard')
+  const [isActive, setIsActive] = useState('dashboard')
   const [toggleDrawer, setToggleDrawer] = useState(false)
   // const { connect, address } = useStateContext();
   return (
@@ -38,6 +38,85 @@ const Navbar = () => {
             // else connect()
           }}
         />
+        <Link to="/profile">
+          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+            <img
+              src={thirdweb}
+              alt="thirdweb"
+              className="w-[60%] h-[60%] object-contain"
+            />
+          </div>
+        </Link>
+      </div>
+
+      {/* Small screen navigation */}
+      <div className="sm:hidden flex justify-between items-center relative">
+        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+          <img
+            src={logo}
+            alt="user"
+            className="w-[60%] h-[60%] object-contain"
+          />
+        </div>
+
+        <img
+          src={menu}
+          alt="menu"
+          className="w-[34px] h-[34px] object-contain cursor-pointer"
+          onClick={() => setToggleDrawer((prev) => !prev)}
+        />
+
+        <div
+          className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${
+            !toggleDrawer ? "-translate-y-[100vh]" : "translate-y-0"
+          } transition-all duration-700`}
+        >
+          <ul className="mb-4">
+            {navlinks.map((link) => (
+              <li
+                key={link.name}
+                className={`flex p-4 ${
+                  isActive === link.name && "bg-[#3a3a43]"
+                }`}
+                onClick={() => {
+                  setIsActive(link.name);
+                  setToggleDrawer(false);
+                  navigate(link.link);
+                }}
+              >
+                <img
+                  src={link.imgUrl}
+                  alt=""
+                  alt={link.name}
+                  className={`w-[24px] h-[24px] object-contain ${
+                    isActive === link.name ? "grayscale-0" : "grayscale"
+                  }`}
+                />
+                <p
+                  className={`ml-[20px] font-epilogue font-semibold text-[14px] ${
+                    isActive === link.name ? "text-[#1dc071]" : "text-[#808191]"
+                  }`}
+                >
+                  {link.name}
+                </p>
+              </li>
+            ))}
+          </ul>
+
+          <div className="flex mx-4">
+            <CustomButton
+              btnType="button"
+              title={"address" ? "Create a campaign" : "Connect"}
+              styles={"address" ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
+              handleClick={() => {
+                // if (address) navigate("create-campaign");
+                // else connect();
+              }}
+            />
+          </div>
+
+
+        </div>
       </div>
     </div>
   );
