794c264a5a795c06a122bdea35b7aa0848de9950
Sylvain T <sylvaintagnabou@gmail.com>
2023-05-24 16:05:17 +0000

    (add): some modif

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 
 import { Navbar, Sidebar } from './components'
 import { Home, Profile, CreateCampaign, CampaignDetails } from './pages'
+import { github } from './assets'
 
 const App = () => {
   return (
@@ -14,11 +15,25 @@ const App = () => {
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         <Navbar />
         <Routes>
-          <Route path='/' element={ <Home /> } />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/create-campaign' element={<CreateCampaign />} />
-          <Route path='/campaign-details/:id' element={<CampaignDetails />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
         </Routes>
+      </div>
+
+      <div className="text-gray-400 absolute bottom-0 right-0 p-4 flex justify-center content-center mt-2">
+        Made & custom By
+        <a
+          href="https://portfolio-sylvat160.vercel.app/"
+          target='_blank'
+          className="text-[#1dc071] pl-1 pr-1"
+        >
+          Sylvain
+        </a>
+        <a href="https://github.com/Sylvat160" target='_blank'>
+          <img src={github} width="25" alt="github" />
+        </a>
       </div>
     </div>
   );
