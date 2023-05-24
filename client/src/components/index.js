7d3486821177218e733172f46d750b5204bf3d73
Sylvain T <sylvaintagnabou@gmail.com>
2023-05-24 12:49:16 +0000

    ...

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,5 @@ export { default as Sidebar } from './Sidebar';
 export { default as Navbar } from './Navbar';
 export { default as CustomButton } from './CustomButtom';
 export { default as FormField } from './FormField';
+export { default as DisplayCampaingns } from './DisplayCampaigns';
+export { default as FundCard } from './FundCard';
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import { useStateContext } from '../context'
+import { DisplayCampaingns } from '../components';
 
 const Home = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const { address, contract, getCampaigns } = useStateContext();
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [address, contract]);
+
   return (
-    <div>Home</div>
+    <DisplayCampaingns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
   )
 }
 
