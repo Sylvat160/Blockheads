080fafa7cf00cdcb98c4dbc422fbec246a5effc3
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2023-06-19 10:33:41 +0200

    changed button text on successfull wallet update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       <div className="sm:flex hidden flex-row justify-end gap-4">
         <CustomButton
           btnType="button"
-          title={address ? "Create a campaign" : "Connect"}
+          title={address ? "Add Property" : "Connect"}
           styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
           handleClick={() => {
             if (address) navigate("create-campaign");
