548e255080193314c5d83f30e5dc00ac25da0246
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2023-06-20 12:15:00 +0200

    updated button route

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
           title={address ? "Add Property" : "Connect"}
           styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
           handleClick={() => {
-            if (address) navigate("create-campaign");
+            if (address) navigate("create-asset");
             else connect()
           }}
         />
