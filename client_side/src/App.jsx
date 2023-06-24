302be50c5e0109c90bdec9d0c6a1e6ae7ff5ecf2
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2023-06-24 12:36:58 +0200

    updated the footer

--- client_side/src/App.jsx
+++ client_side/src/App.jsx
@@ -28,11 +28,11 @@ const App = () => {
       <div className="text-gray-400 absolute bottom-0 right-0 p-4 flex justify-center content-center mt-2">
         Made & custom By
         <a
-          href="https://portfolio-sylvat160.vercel.app/"
+          href="https://tokenhive.vercel.app/"
           target='_blank'
           className="text-[#1dc071] pl-1 pr-1"
         >
-          Sylvain
+          The Blockheads
         </a>
         <a href="https://github.com/Sylvat160" target='_blank'>
           <img src={github} width="25" alt="github" />
